c304b87f893471bc5478da541c021275abe4ccd9
Gruzin <gruzd.roman@inbox.ru>
2023-03-07 21:02:58 +0300

    Commit with a solution

--- tasks/task.py
+++ tasks/task.py
@@ -5,4 +5,9 @@ NumType = Union[int, float]
 def some_expression_with_rounding(a: NumType, b: NumType) -> NumType:
   result = None
   # add your code here
+  result = round((12 * a + 25 * b) / (1 + a ** (2 ** b)), 2)
+
   return result
+
+
+
